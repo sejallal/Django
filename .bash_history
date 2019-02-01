aa7702ecf492daa295a097893f9af412943b6a47
sejallal <sejallal@umich.edu>
2019-02-01 23:39:19 +0000

    first commit

---
+++ .bash_history
@@ -0,0 +1,25 @@
+workon django2
+cd django_projects/loallibrary/
+cd django_projects/locallibrary/
+nano catalog/models.py  
+python3 manage.py makemigrations
+python3 manage.py migrate
+sqlite3 db.sqlite3 
+vi catalog/models.py
+python3 manage.py makemigrations
+vi catalog/models.py 
+python3 manage.py makemigrations
+vi catalog/models.py 
+python3 manage.py makemigrations
+python3 manage.py migrate
+sqlite3 db.sqlite3 
+cd django_projects/
+
+vi catalog/admin.py 
+python3 manage.py createsuperuser
+python3 manage.py runserver
+vi catalog/admin.py 
+vi catalog/models.py 
+python3 manage.py createsuperuser
+vi manage.py 
+python3 manage.py createsuperuser
---
+++ django_projects/locallibrary/catalog/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
+    path('author/<int:pk>', views.AuthorDetailView.as_view(), name= 'author-detail'),
+]
+
+import os
+from django.urls import re_path
+from django.views.static import serve
+from django.conf import settings
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+if settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^static/(?P<path>.*)$', serve, {
+            'document_root': os.path.join(BASE_DIR, 'catalog/static'),
+        }),
+    ]
+
+
